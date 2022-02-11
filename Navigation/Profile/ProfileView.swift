02780ae8e0d23825dda4f5d974fe6d58b20cdbed
Danylo Drogachov <drogachov.gta@gmail.com>
2022-02-11 19:35:13 +0100

    HW1

---
+++ Navigation/Profile/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Danylo Drogachov on 10.02.22.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var userAvatarView: UIImageView!
+    
+    @IBOutlet weak var userNameView: UILabel!
+    
+    @IBOutlet weak var userBirthDayView: UILabel!
+    
+    @IBOutlet weak var userCityView: UILabel!
+    
+    @IBOutlet weak var userTextView: UITextView!
+    
+}
