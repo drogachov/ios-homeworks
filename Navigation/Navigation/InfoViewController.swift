bc6d912c9f261d1118164d84850cf7ff44a7de09
Danylo Drogachov <drogachov.gta@gmail.com>
2022-02-20 16:20:27 +0100

    Button, Alert Controller and Alert Action done.

--- Navigation/Navigation/InfoViewController.swift
+++ Navigation/Navigation/InfoViewController.swift
@@ -11,20 +11,27 @@ class InfoViewController: ViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBackground
+        self.view.backgroundColor = .purple
+        let button = UIButton(frame: CGRect(x: 120, y: 300, width: 200, height: 50))
+        button.setTitle("Don‘t click on me!", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(click), for: .touchUpInside)
+        
+        self.view.addSubview(button)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func click() {
+        let alertVC = UIAlertController(title: "Warning!", message: "Call your mom", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "Call", style: .default, handler: { action in print("Call worked")})
+        let action2 = UIAlertAction(title: "Remind me", style: .destructive, handler: { action in print("Remind me worked")})
+        alertVC.addAction(action1)
+        alertVC.addAction(action2)
+        
+        self.present(alertVC, animated: true, completion: nil)
+        
     }
-    */
 
 }
+
