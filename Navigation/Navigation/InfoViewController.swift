62680ce3db3fea5f62688f566d0e462f1895b2b2
Danylo Drogachov <drogachov.gta@gmail.com>
2022-02-20 14:30:37 +0100

    Bar Button Item added

---
+++ Navigation/Navigation/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  Navigation
+//
+//  Created by Danylo Drogachov on 20.02.22.
+//
+
+import UIKit
+
+class InfoViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Navigation/Navigation/PostViewController.swift
+++ Navigation/Navigation/PostViewController.swift
@@ -13,11 +13,20 @@ class PostViewController: ViewController {
         super.viewDidLoad()
         self.title = News.title
         self.view.backgroundColor = .orange
-
+        
+        let infoButton = UIButton(type: .infoLight)
+        infoButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        let barItem = UIBarButtonItem(customView: infoButton)
+        self.navigationItem.rightBarButtonItem = barItem
+        
         // Do any additional setup after loading the view.
     }
     
-    
-  
+    @objc func tap() {
+        let vc = InfoViewController()
+        self.present(vc, animated: true, completion: nil)
+    }
+        
+        
 
 }
